3337e74ca034d354a5e92955091deb734d0a4cbc
haciyev13 <ehedhaciyev001@gmail.com>
2023-11-16 16:35:37 +0400

    Task Add

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,53 +5,110 @@ let main = document.querySelector(".country-detail-container");
 let back = document.querySelector(".link-back");
 let darkModeTheme = document.querySelector(".dark-mode-theme");
 let body = document.querySelector("body");
+let filterSelect = document.querySelector(".filter-selected");
+let ionIcon = document.querySelector(".ion-icon");
+let filterOptions = document.querySelector(".filter-options");
+let filterOpt = document.querySelectorAll(".filter-option");
+let inputCountries = document.getElementById("country");
+let allOpt = document.querySelector("all-options");
 
-// if () {
+let countriesArray = [];
+
+let mode = localStorage.getItem("theme");
+console.log(mode);
+if (mode === "true") {
+  body.classList.add("dark-mode");
+} else {
+  body.classList.remove("dark-mode");
+}
+darkModeTheme.addEventListener("click", function (e) {
+  e.preventDefault();
+  body.classList.toggle("dark-mode");
+  mode === "true" ? (mode = "false") : (mode = true);
+  console.log(mode);
+  localStorage.setItem("theme", mode);
+});
+
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((datas) => {
     // console.log(datas);
-    datas.map((data) => {
-      let ancor = document.createElement("a");
-      ancor.className = "country-card-container";
-      ancor.href = `/country-details.html?name=${data.name.common}`;
-      ancor.innerHTML = `
-            <div class="country-card">
-                <div class="card-img-container">
-                  <img src="${data.flags.png}" alt="country-img" />
-                </div>
-
-              <div class="card-content">
-                <p class="card-header">${data.name.common}</p>
-
-                <div class="card-info">
-                  <p>Population: <span>${data.population.toLocaleString(
-                    "en-UK"
-                  )}</span></p>
-                  <p class="region-name">Region: <span>${data.region}</span></p>
-                  <p>Capital: <span>${data.capital?.[0] || "None"}</span></p>
-                </div>
-              </div>
-            </div>
-            `;
-      mainContainer.appendChild(ancor);
-    });
+    countriesArray = datas;
+    createCountries(datas);
   });
 // }
-  let mode = localStorage.getItem("theme");
-  console.log(mode)
 
-  if (mode==='true') {
-    body.classList.add("dark-mode")
-  }else{
-    body.classList.remove("dark-mode")
+ionIcon.style.transform = "rotate(180deg)";
+filterSelect.addEventListener("click", function (e) {
+  if (!filterOptions.classList.contains("active")) {
+    filterOptions.classList.add("active");
+    ionIcon.style.transform = "rotate(0deg)";
+    ionIcon.style.transition = "All .5s";
+    filterOptions.style.transition = "All .9s";
+  } else {
+    filterOptions.classList.remove("active");
+    ionIcon.style.transform = "rotate(180deg)";
   }
-darkModeTheme.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  body.classList.toggle("dark-mode");
-  mode === "true"? mode="false": mode=true;
-  console.log(mode);
-
-  localStorage.setItem("theme", mode);
 });
+
+filterOpt.forEach((item) => {
+  item.addEventListener("click", function () {
+    console.log(item);
+    filterSelect.firstElementChild.textContent = item.textContent;
+    filterOptions.classList.remove("active");
+    ionIcon.style.transform = "rotate(180deg)";
+    console.log(filterCountries(filterSelect.firstElementChild.textContent));
+  });
+});
+
+function filterCountries(region) {
+  mainContainer.innerHTML = "";
+  fetch(`https://restcountries.com/v3.1/region/${region}`)
+    .then((res) => res.json())
+    .then((datas) => {
+      countriesArray = datas;
+      createCountries(datas);
+      allOpt.addEventListener("click", function () {
+        window.location.href = "/index.html";
+      });
+      
+    });
+}
+
+function createCountries(datas) {
+  mainContainer.innerHTML = "";
+  datas.map((data) => {
+    let ancor = document.createElement("a");
+    ancor.className = "country-card-container";
+    ancor.href = `/country-details.html?name=${data.name.common}`;
+    ancor.innerHTML = `
+        <div class="country-card">
+            <div class="card-img-container">
+              <img src="${data.flags.png}" alt="country-img" />
+            </div>
+
+          <div class="card-content">
+            <p class="card-header">${data.name.common}</p>
+
+            <div class="card-info">
+              <p>Population: <span>${data.population.toLocaleString(
+                "en-UK"
+              )}</span></p>
+              <p class="region-name">Region: <span>${data.region}</span></p>
+              <p>Capital: <span>${data.capital?.[0] || "None"}</span></p>
+            </div>
+          </div>
+        </div>
+        `;
+    mainContainer.appendChild(ancor);
+  });
+}
+
+inputCountries.addEventListener("keyup", function (e) {
+  let countreisFiltered = countriesArray.filter((country) => {
+    return country.name.common.toLowerCase().includes(e.target.value);
+  });
+  console.log(countreisFiltered);
+  createCountries(countreisFiltered);
+});
+
--- assets/js/details.js
+++ assets/js/details.js
@@ -2,40 +2,70 @@ let countryName = new URLSearchParams(document.location.search).get("name");
 
 let detailImg = document.querySelector(".details-img");
 let detailtitle = document.querySelector(".detail-title");
-let detailNativeName = document.querySelector(".details-native-name");
-let detailPopulation = document.querySelector(".details-population");
+let detailNativeName = document.querySelector(".detail-native-name");
+let detailPopulation = document.querySelector(".detail-population");
 let detailRegion = document.querySelector(".detail-region");
 let detailSubRegion = document.querySelector(".detail-sub-region");
 let detailCapital = document.querySelector(".detail-capital");
 let detailDomain = document.querySelector(".detail-domain");
 let detailcurrent = document.querySelector(".detail-current");
 let detailLang = document.querySelector(".detail-lang");
-let body = document.querySelector(".body")
+let body = document.querySelector(".body");
+let borderCountry = document.querySelector(
+  ".border-countries-container"
+).lastElementChild;
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}`)
   .then((res) => res.json())
   .then((data) => {
-    data.map((country) => {
+    data.forEach((country) => {
       console.log(country);
       detailImg.src = country.flags.svg;
       detailtitle.innerHTML = country.name.common;
-      console.log(Object.values(country.name.nativeName));
       if (country.name.nativeName) {
         detailNativeName.innerHTML = `Native Name: <span>${
           Object.values(country.name.nativeName)[0].common
         }</span>`;
       } else {
-        detailNativeName.innerHTML = `Native Name: <span>${country.name.common}</span>`;
+        Object.values(country.name.nativeName)[0].common;
+      }
+      detailPopulation.innerHTML = `Population: <span>${country.population.toLocaleString()}</span>`;
+      detailSubRegion.innerHTML = `Sub Region: <span>${
+        country.subregion ? country.subregion : "None"
+      }</span>`;
+      detailRegion.innerHTML = `Region: <span>${country.region}</span>`;
+      detailCapital.innerHTML = `Capital: <span>${
+        country.capital?.[0] || "None"
+      }</span>`;
+      detailDomain.innerHTML = `Top Level Domain: <span>${country.tld}</span>`;
+      // console.log(Object.values(country.currencies)[0].name);
+      detailcurrent.innerHTML = `Currencies: <span>${
+        Object.values(country.currencies)[0].name
+      }</span>`;
+      detailLang.innerHTML = `Languages: <span>${Object.values(
+        country.languages
+      )}</span>`;
+
+      if (country.borders) {
+        country.borders.forEach((elem) => {
+          fetch(`https://restcountries.com/v3.1/alpha/${elem}`)
+            .then((res) => res.json())
+            .then((data) => {
+              let ancorTag = document.createElement("a");
+              ancorTag.href = `/country-details.html?name=${data[0].name.common}`
+              ancorTag.innerHTML = data[0].name.common;
+              borderCountry.appendChild(ancorTag)
+            });
+        });
       }
     });
   });
 
+let local = localStorage.getItem("theme");
+console.log(local);
 
-  let local = localStorage.getItem("theme")
-  console.log(local)
-  
-  if (local==='true') {
-    body.classList.add("dark-mode")
-  }else{
-    body.classList.remove("dark-mode")
-  }
+if (local === "true") {
+  body.classList.add("dark-mode");
+} else {
+  body.classList.remove("dark-mode");
+}
